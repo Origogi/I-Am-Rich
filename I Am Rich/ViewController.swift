5e0a944d9be998ce0d375816e8a3d09b378c6c06
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2020-03-11 15:30:57 +0900

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by 1101373 on 2020/03/11.
+//  Copyright © 2020 1101373. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
